afe2f51868dcc6b601fa28572072264c2c8bb2ac
Erik sales <58636303+ErickSousa312@users.noreply.github.com>
2023-03-10 00:17:48 -0300

    update fast

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,60 @@
-import '@/styles/globals.css'
+import { useState } from 'react';
+import Head from 'next/head';
+import Link from 'next/link';
+import '../styles/globals.css';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!sidebarOpen);
+  };
+
+  return (
+    <>
+      <Head>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
+      </Head>
+      <div className={`wrapper ${sidebarOpen ? 'sidebar-open' : 'sidebar-collapse'}`}>
+        <nav className="sidebar bg-dark">
+          <div className="sidebar-header">
+            <h3>My App</h3>
+          </div>
+          <ul className="list-unstyled components">
+            <li>
+              <Link href="/">
+                 Home
+              </Link>
+            </li>
+            <li>
+              <Link href="/about">
+                About
+              </Link>
+            </li>
+            <li>
+              <Link href="/contact">
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <div id="content">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <button
+              className="navbar-toggler"
+              type="button"
+              onClick={toggleSidebar}
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+          </nav>
+          <div className="container-fluid">
+            <Component {...pageProps} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
+
+export default MyApp;
